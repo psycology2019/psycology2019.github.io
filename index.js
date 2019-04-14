cdac67aa49d72b25f562651e9e0ecf1703a7f5db
psycology2019 <47485609+psycology2019@users.noreply.github.com>
2019-04-14 12:24:55 +0300

    all

--- index.js
+++ index.js
@@ -7,6 +7,8 @@
         event.preventDefault();
     });
 	console.log('ghbdtn');
+	
+
 });
 
 // Highlight the top nav as scrolling occurs
@@ -19,3 +21,20 @@ $('.navbar-collapse ul li a').click(function() {
     $('.navbar-toggle:visible').click();
 	console.log('1');
 });
+
+
+var btn = $('#button');
+
+$(window).scroll(function() {
+  if ($(window).scrollTop() > 300) {
+    btn.addClass('show');
+  } else {
+    btn.removeClass('show');
+  }
+});
+
+btn.on('click', function(e) {
+  e.preventDefault();
+  $('html, body').animate({scrollTop:0}, '300');
+});	
+	
