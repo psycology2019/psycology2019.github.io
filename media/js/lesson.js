9b36b23c6894617c804b5f435b3eaac4470b574f
psycology2019 <47485609+psycology2019@users.noreply.github.com>
2019-04-16 02:46:12 +0300

    all

---
+++ media/js/lesson.js
@@ -0,0 +1,20 @@
+﻿$(function() {
+$(".ripple").on("click",function(event){
+  
+$(this).append("<span class='ripple-effect'>");
+$(this).find(".ripple-effect").css({
+   left:event.pageX-$(this).position().left,
+    top:event.pageY-$(this).position().top
+  }).animate({
+    opacity: 0,
+  }, 1500, function() {
+   $(this).remove();
+  });
+});	
+	
+$('.btn').click(function(){
+  $(".form_lesson").fadeToggle(100);
+});  
+
+});
+
