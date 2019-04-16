b06bf5444138031efb16de9a706586c0099d88e2
psycology2019 <47485609+psycology2019@users.noreply.github.com>
2019-04-17 02:32:18 +0300

    all

--- button_up.js
+++ button_up.js
@@ -1,17 +1,22 @@
-﻿
-var btn = $('#button');
-
-$(window).scroll(function() {
-  if ($(window).scrollTop() > 100) {
-    btn.addClass('show');
-  } else {
-    btn.removeClass('show');
-	console.log("button")
-  }
+﻿$(document).ready(function(){
+  $('body').append('<a href="#" id="go-top" title="Вверх">Вверх</a>');
 });
 
-btn.on('click', function(e) {
-  e.preventDefault();
-  $('html, body').animate({scrollTop:0}, '100');
-});	
+$(function() {
+ $.fn.scrollToTop = function() {
+  $(this).hide().removeAttr("href");
+  if ($(window).scrollTop() >= "250") $(this).fadeIn("slow")
+  var scrollDiv = $(this);
+  $(window).scroll(function() {
+   if ($(window).scrollTop() <= "250") $(scrollDiv).fadeOut("slow")
+   else $(scrollDiv).fadeIn("slow")
+  });
+  $(this).click(function() {
+   $("html, body").animate({scrollTop: 0}, "slow")
+  })
+ }
+});
 
+$(function() {
+ $("#go-top").scrollToTop();
+});
